29e9f669c0b74a7e6c4a324699843bb2a95a0099
Ivan <ivanov399@mail.ru>
2024-02-04 19:47:53 +0300

    Initial commit

--- myproject/telegram.py
+++ myproject/telegram.py
@@ -1,9 +1,9 @@
 import requests
-TOKEN = "6380594023:AAEE2LT3trZR6mydrPCX0Djyd0COs-v9QwU"
+TOKEN = "TOKEN"
 url = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
 print(requests.get(url).json())
 
-chat_id = '5401729924'
+chat_id = 'chat_id'
 message = "Привет, Иван"
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
 
